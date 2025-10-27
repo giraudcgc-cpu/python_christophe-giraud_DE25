51406289f500de0c17192d1ab722fcaf3463c623
giraudc - cpu <giraudcgc@gmail.com>
2025-10-27 12:29:59 +0100

    Added operators overloading, not done, many questions

--- lab2/shape_parent_base.py
+++ lab2/shape_parent_base.py
@@ -20,8 +20,40 @@ def x(self):
 def y(self):
     return self._y
 
-#area and perimeter
-#get all the methods(), drawing?
-        
-        
-    
+# area or perimeter here as "common operations" or under the children as split perimeter/area?
+#def area(self):
+#    self.are = area
+#    pass
+#def perimeter(self):
+#    self.perimeter = perimeter
+#    pass
+
+
+def __eq__(self, other):
+    if not isinstance(other, type(self)): # if other is not the same type as self (circle or rectangle)
+        return False
+    return self._x = other._x and self._y = other._y 
+
+def __ge__(self, other): # >= Greater than or equal to ???comparing areas or perimeter?
+    if not isinstance(other, type(Shapes)): # if other is not a type of shape
+        return f"You cannot compare apples and oranges!"
+    return #??? not sure >=
+
+def __le__(self, other):   # <= Less than or equal to
+    if not isinstance(other, type(Shapes)):
+        return f"You cannot compare apples and oranges"
+    return #??? <= size/area to compare???
+
+def __gt__(self, other): 
+    if not isinstance(other, type(Shapes)):
+        return f"You cannot compare apples and oranges"
+    return # self.area > other.area or self.periimeter > other.perimeter ???
+
+def __lt__(self, other):
+    if not isinstance(other, type(Shapes)):
+        return f"You cannot compare apples and oranges"
+    return # self.area < other.area or .perimeter ???
+
+
+
+
